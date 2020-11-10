cf33344802151d006819ee19f46eac0949ffce0c
W0L1D <ahdoufwalid@gmail.com>
2020-11-10 17:23:38 +0100

    commit

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    // res.send("<h1>Hello World!</h1>")
-    res.render('index')
+    res.send("<h1>Hello World!</h1>")
+    //res.render('index')
 })
 
 
