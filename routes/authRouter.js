01a8525925ebdb52f1143443e883bfab269f7193
W0L1D <ahdoufwalid@gmail.com>
2020-11-11 21:12:47 +0100

    dsf er

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -16,30 +16,39 @@ router.get('/login', (req, res) => {
 
 router.post('/register', async (req, res) => {
 
-    //  validate the data before create a user
-    const { error } = registerValidation(req.body)
-    if (error) return res.status(400).send(error);
-
-    const usernameExists = await User.findOne({username:req.body.username})
-    if (usernameExists) return res.status(400).send({error:{field:'username', message:'Username Already Exists'}})
-
-    const emailExists = await User.findOne({prEmail:req.body.email})
-    if (emailExists) return res.status(400).send({error:{field:'email', message:'Email Already Exists'}})
-
-
-
-    const user = new User({
-        username: req.body.username,
-        prEmail: req.body.email,
-        password: await encrypter.encrypt(req.body.password),
-        sendNews: req.body.sendNews
-    })
-
     try {
-        const savedUser = await user.save();
-        res.status(201).send(savedUser)
+
+        //  validate the data before create a user
+        const {error} = registerValidation(req.body)
+        if (error) return res.status(400).send(error);
+
+        const usernameExists = await User.findOne({username: req.body.username})
+        if (usernameExists) return res.status(400).send({
+            error: {
+                field: 'username',
+                message: 'Username Already Exists'
+            }
+        })
+
+        const emailExists = await User.findOne({prEmail: req.body.email})
+        if (emailExists) return res.status(400).send({error: {field: 'email', message: 'Email Already Exists'}})
+
+
+        const user = new User({
+            username: req.body.username,
+            prEmail: req.body.email,
+            password: await encrypter.encrypt(req.body.password),
+            sendNews: req.body.sendNews
+        })
+
+        try {
+            const savedUser = await user.save();
+            res.status(201).send(savedUser)
+        } catch (error) {
+            res.status(500).send(error)
+        }
     } catch (error) {
-        res.status(500).send(error)
+        res.send(error)
     }
 })
 
