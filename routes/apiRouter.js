e560a04b8d57a7cd92cd995c46905787b0af24b9
W0L1D <ahdoufwalid@gmail.com>
2020-11-11 18:14:20 +0100

    dsf er

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
+const auth = require('auth');
+
 
 router.get('/', (req, res, next) => {
     // res.send("<h1>Hello World!</h1>")
     res.render('index')
 })
 
-
+app.use('/register', auth)
 
 
 module.exports = router
--- routes/auth.js
+++ routes/auth.js
@@ -32,6 +32,6 @@ router.post('api/register', async (req, res) => {
     } catch (error) {
         res.status(500).send(error)
     }
-
-
 })
+
+module.exports = router;
