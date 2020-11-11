57e37ca833d3b87b6df519c2479968999021f615
W0L1D <ahdoufwalid@gmail.com>
2020-11-11 21:29:48 +0100

    dsf er

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 const User = require('../models/User')
-const { registerValidation } = require('../validations')
-const { encrypt } = require('../encrypter')
 
 router.get('/register', (req, res) => {
     res.render('register')
@@ -17,10 +15,11 @@ router.get('/login', (req, res) => {
 router.post('/register', async (req, res) => {
 
     res.send("hello world!")
-    //  validate the data before create a user
-    const {error} = registerValidation(req.body)
-    if (error) return res.status(400).send(error);
 
+    //  validate the data before create a user
+//    const {error} = registerValidation(req.body)
+//    if (error) return res.status(400).send(error);
+/*
     const usernameExists = await User.findOne({username: req.body.username})
     if (usernameExists) return res.status(400).send({
         error: {
@@ -39,9 +38,9 @@ router.post('/register', async (req, res) => {
         password: await encrypt(req.body.password),
         sendNews: req.body.sendNews
     })
-
+*/
     try {
-        const savedUser = await user.save();
+        const savedUser = await User.save();
         res.status(201).send(savedUser)
     } catch (error) {
         res.status(500).send(error)
